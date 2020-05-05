c9f609c83202612eb2b73a5180999f5a28cbf296
Sola Salami <solasalami@gmail.com>
2020-05-05 22:05:53 +0200

    updated code with routing and flitering

--- Lesson01/Ass03/app.js
+++ Lesson01/Ass03/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 
-app.use("/users",(req,res,next) => {
-    console.log("I am in Middleware 2");
-    res.send("<h3>Users List </h3>");
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found !</h1>');
 });
-
-
-app.use("/",(req,res,next) => {
-    console.log("I am in Middleware 1");
-    res.send("<h3>Home Page </h3>");
-});
-
-
 app.listen(4000);
 
 console.log("Server Started ...");
